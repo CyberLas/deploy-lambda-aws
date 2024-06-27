13bbf15cc7e1fa3f080a385a4a41ce93113ea430
Carlos Angeles <caamnil@gmail.com>
2024-06-27 00:46:20 -0500

    Agrgegando esto

---
+++ functions/api.js
@@ -0,0 +1,31 @@
+const cors			= require("cors")
+const express		= require("express")
+const serverless	= require("serverless-http")
+
+let app = express()
+app.use(cors())
+
+const router = express.Router()
+
+app.get("/", (req, res) => {
+	console.log("/")
+	res.status(200).json({
+		status:200,
+		response: 'Ok'
+	})
+})
+
+
+router.get("/si", (req, res) => {
+	console.log("//")
+	res.status(200).json({
+		status:200,
+		response: 'Ok'
+	})
+})
+
+
+app.use('/.netlify/functions/api', router)
+
+module.exports.handler = serverless(app)
+// app.listen(port);
