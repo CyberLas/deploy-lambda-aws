6d34c8bb1bb5444a9ead28f20f2720c290828955
Carlos Angeles <caamnil@gmail.com>
2024-06-26 23:08:10 -0500

    Agrgegando esto

--- functions/send-message.js
+++ functions/send-message.js
@@ -1,9 +1,7 @@
-const handler = async function(event, context) {
+export const handler = async function(event, context) {
   console.log("Received event:", event)
 
   return {
       statusCode: 200,
   };
 };
-
-module.exports.handler = handler;
