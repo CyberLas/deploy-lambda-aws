cf5cc43408c1ad7a938f2d6f3ccddb8ab6693c49
Carlos Angeles <caamnil@gmail.com>
2024-06-26 23:44:05 -0500

    Agrgegando esto

--- functions/send-message.js
+++ functions/send-message.js
@@ -5,10 +5,19 @@ const serverless = require("serverless-http")
 var app = express()
 app.use(cors())
 
-var port = process.env.PORT || 5000
+// var port = process.env.PORT || 5000
 const router = express.Router()
 
-router.get("/my-top-5-power-rangers-seasons", function (req, res) {
+router.get("/", function (req, res) {
+	console.log("hi wordl")
+	res.json({
+		status:200,
+		response: 'Ok'
+	})
+})
+
+
+router.get("/si", function (req, res) {
 	console.log("hiiii")
 	res.json({
 		status:200,
