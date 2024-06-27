36fca54822be7fa3c07b5ccbcad59ac5a39d4a72
Carlos Angeles <caamnil@gmail.com>
2024-06-27 02:03:02 -0500

    Agrgegando esto

--- functions/api.js
+++ functions/api.js
@@ -1,20 +1,6 @@
 const	cors		= require("cors")
 const	express		= require("express")
-const	cron		= require('node-cron')
 const	serverless	= require("serverless-http")
-const	pgPromise	= require('pg-promise')
-		pgp			= pgPromise({ schema: "BDLogSoporte" }),
-		postgresDB	= pgp({
-			host					:'ep-rough-hat-a5n0tgnh.us-east-2.aws.neon.tech',
-			port					:5432,
-			database				:'culqiTest',
-			user					:'culqiTest_owner',
-			password				:'XD4Fja9lqdMS',
-			max						: 30,
-			ssl						: true,
-			idleTimeoutMillis		: 360000,
-			connectionTimeoutMillis	: 360000
-		})
 
 let app = express()
 app.use(cors())
@@ -40,12 +26,7 @@ router.get("/si", (req, res) => {
 	})
 })
 
-const npmTest = async() => {
-	console.log("WRITE")
-	await postgresDB.oneOrNone(`INSERT INTO test(test) VALUES($1)`, ['hi']).then((res) => res)
-}
 
-cron.schedule('*/5 * * * * *', npmTest)
 app.use('/.netlify/functions/api', router)
 
 module.exports.handler = serverless(app)
