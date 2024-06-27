0ca12167d3e5ec95b2be6b2bd717ff457a64b5b8
Carlos Angeles <caamnil@gmail.com>
2024-06-26 22:58:12 -0500

    Agregando esto

--- functions/send-message.js
+++ functions/send-message.js
@@ -1,18 +1,9 @@
+const handler = async function(event, context) {
+  console.log("Received event:", event)
 
-exports.handler = async (event, context) => {
-  try {
-    console.log('Mensaje enviado correctamente!');
-
-    return {
+  return {
       statusCode: 200,
-      body: JSON.stringify({ message: 'Mensaje enviado correctamente' })
-    };
-  } catch (error) {
-    console.error(error);
-
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: 'Error al enviar el mensaje' })
-    };
-  }
+  };
 };
+
+module.exports.handler = handler;
